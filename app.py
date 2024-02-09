01176c2766d2ef46b65032f93f70aa35bff9d429
NobleJoe123 <145128278+NobleJoe123@users.noreply.github.com>
2024-02-09 13:17:26 +0100

    add

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 
 app = Flask(__name__)
@@ -6,6 +6,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/shop', methods=['GET', 'POST'])
+def cool_form():
+    if request.method == 'POST':
+        return redirect(url_for('index'))
+
+    # show the form, it wasn't submitted
+    return render_template('cool_form.html'
 
 if __name__ == '__main__':
     app.run(debug=True)
