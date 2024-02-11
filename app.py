2c0536403ff6c8c5088eb88db2ca84f77d4dc5b3
NobleJoe123 <145128278+NobleJoe123@users.noreply.github.com>
2024-02-11 16:12:31 +0100

    add

--- app.py
+++ app.py
@@ -3,15 +3,17 @@ from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        return redirect(url_for('index'))    
     return render_template('index.html')
 
 
 @app.route('/shop', methods=['GET', 'POST'])
 def shop():
     if request.method == 'POST':
-        return redirect(url_for('index'))
+        return redirect(url_for('shop'))
     return render_template('shop.html')
 
 @app.route('/checkout', methods=['GET', 'POST'])
