00876fd78ab404e5199dc5efb69de96e4f9a5079
NobleJoe123 <145128278+NobleJoe123@users.noreply.github.com>
2024-02-11 16:28:08 +0100

    add

--- app.py
+++ app.py
@@ -3,10 +3,8 @@ from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        return redirect(url_for('index'))    
+@app.route('/')
+def index():   
     return render_template('index.html')
 
 
