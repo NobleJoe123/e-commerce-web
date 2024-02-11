ea13a143ad0203188f6b3590be2c5cfbfe0abe20
NobleJoe123 <145128278+NobleJoe123@users.noreply.github.com>
2024-02-11 16:48:29 +0100

    add

--- app.py
+++ app.py
@@ -28,10 +28,10 @@ def checkout():
         return redirect(url_for('checkout'))
     return render_template('chackout.html')
 
-@app.route('/shopdetail', methods=['GET', 'POST'])
+@app.route('/shop-detail', methods=['GET', 'POST'])
 def shopdetail():
     if request.method == 'POST':
-        return redirect(url_for('shopdetail'))
+        return redirect(url_for('shop-detail'))
     return render_template('shop-detail.html')
 
 @app.route('/testimonial', methods=['GET', 'POST'])
