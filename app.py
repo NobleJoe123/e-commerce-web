08732309bab3fd8f9c50eccfd52eb70c8718e4bb
NobleJoe123 <145128278+NobleJoe123@users.noreply.github.com>
2024-02-13 15:41:17 +0100

    add

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request,Response, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+
+
 
 
 app = Flask(__name__)
+
+app.secret_key = 'key'
+
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'commerce'
+
+
+mysql = MySQL(app)
+
+
 
 @app.route('/')
 def index():   
@@ -55,6 +72,54 @@ def cart():
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
+@app.route('/login/index')
+def home():
+    if 'loggedin' in session:
+        return render_template('index.html', username=session['username'])
+    return redirect(url_for('/log'))
+
+@app.route('/')
+def signup():
+    return render_template('register.html')
+     
+
+@app.route('/reg', methods=['GET', 'POST'])
+def reg():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        cursor = mysql.connection.cursor()
+        cursor.execute('INSERT INTO web VALUES (NULL, %s, %s, %s)', (username, password, email,))
+        mysql.connection.commit()
+        msg = 'You have successfully registered!'
+        return render_template('login.html', msg=msg)
+    elif request.method == 'POST':
+        msg = 'Please fill out the form!'
+    return render_template('register.html', msg=msg)
+    
+@app.route('/log', methods=['GET', 'POST'])
+def log():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM web WHERE username = % s AND password = % s', (username, password, ))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['Id'] = account['id']
+            session['Username'] = account['username']
+            msg = 'Logged in successfully !'
+            return render_template('index.html', msg = 'username')
+        else:
+            msg = 'Incorrect username / password !'
+    return render_template('login.html', msg = msg)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
