e9e3c0a292d79108d57c09afe36627fc7efc2a57
NobleJoe123 <145128278+NobleJoe123@users.noreply.github.com>
2024-02-13 13:07:06 +0100

    add

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def cart():
 
  @app.errorhandler(404)
  def page_not_found(e):
-#  return render_template('404.html'), 404
+     return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
