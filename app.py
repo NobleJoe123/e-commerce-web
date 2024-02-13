e8b9a1298792c6221a6d93ace950cad42101107b
NobleJoe123 <145128278+NobleJoe123@users.noreply.github.com>
2024-02-13 13:06:35 +0100

    add

--- app.py
+++ app.py
@@ -52,6 +52,9 @@ def cart():
         return redirect(url_for('cart'))
     return render_template('cart.html')
 
+ @app.errorhandler(404)
+# def page_not_found(e):
+#  return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
